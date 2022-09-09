6d5c2bd03cd47c6ca2a7e2338548870065e0c068
steelfreak <kinenkey@gmail.com>
2022-09-09 11:07:44 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 /**
 * main - print the string in the put function
 *
-*Desctiption: using the main function 
+*Desctiption: using the main function
 *this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-       	return (0);
+	return (0);
 }

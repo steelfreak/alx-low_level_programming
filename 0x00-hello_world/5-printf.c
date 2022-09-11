53d23409030d7f2cc4f525c0a0b1268627ba87b9
steelfreak <kinenkey@gmail.com>
2022-09-11 09:03:47 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * main - prints the phrase with proper grammar,
  * but the outcome is a piece of art,
@@ -8,4 +10,5 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
 

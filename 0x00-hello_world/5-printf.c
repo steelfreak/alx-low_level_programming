0fc0bf82f151a0359fb2789776d2476209919be1
steelfreak <kinenkey@gmail.com>
2022-09-11 09:00:37 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
-#include <stdio.h>
-
 /**
- *main - print the string in the put function
- *
- *Description:using the main function
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
-
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
+

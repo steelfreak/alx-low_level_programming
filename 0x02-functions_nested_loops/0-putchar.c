3d1d7b5101f60a0844a67bcaca15240549a4dfb7
steelfreak <kinenkey@gmail.com>
2022-09-13 09:43:31 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar
+ *
+ * Return: return 0 always success
+*/
+int main(void)
+{
+	/*call _putchar function to print single characters*/
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
+
+

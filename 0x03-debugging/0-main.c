43b4836e2b5844c5ebc0db8e9d2e8cf91c663fe5
steelfreak <kinenkey@gmail.com>
2022-09-14 12:26:12 -0700

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
 nclude "main.h"
 
 /**
- *   * main - Test function for positive or negative
- *     * Return: 0
- *       */
+ * main - Test function for positive or negative
+ * Return: 0
+ */
 
 int main(void)
 {

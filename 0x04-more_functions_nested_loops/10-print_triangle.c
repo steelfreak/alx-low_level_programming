c25e94e9d9681a9a1305cd987f5f696851dd9520
steelfreak <kinenkey@gmail.com>
2022-09-15 11:35:38 -0700

    10. a function that prints a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,26 +4,64 @@
 
 /**
 
- * main - check the code
+ * print_triangle - entry point
 
  *
 
- * Return: Always 0.
+ * Description: Prints diagonals
+
+ *@size: size of the triangle
+
+ * Return: void
 
  */
 
-int main(void)
+
+
+void print_triangle(int size)
 
 {
 
-    print_triangle(2);
+	int row, hashes, spaces;
 
-    print_triangle(10);
 
-    print_triangle(1);
 
-    print_triangle(0);
+	if (size <= 0)
 
-    return (0);
+	{
+
+		_putchar('\n');
+
+	}
+
+	else
+
+	{
+
+		for (row = 1; row <= size; row++)
+
+		{
+
+			for (spaces = size - row; spaces >= 1; spaces--)
+
+			{
+
+				_putchar(' ');
+
+			}
+
+			for (hashes = 1; hashes <= row; hashes++)
+
+			{
+
+				_putchar('#');
+
+			}
+
+			_putchar('\n');
+
+		}
+
+	}
 
 }

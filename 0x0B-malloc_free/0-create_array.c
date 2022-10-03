2a36b5e08c19b445c900c6e90c308b2c360f0eb2
steelfreak <kinenkey@gmail.com>
2022-10-03 11:09:52 -0700

    Task 1.  a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,19 +9,18 @@
  */
 char *create_array(unsigned int size, char c)
 {
-  char *cr;
-  unsigned int i;
+char *cr;
+unsigned int i;
 
-  if (size == 0)
-    return (NULL);
+if (size == 0)
+return (NULL);
 
-  cr = malloc(sizeof(c) * size);
+cr = malloc(sizeof(c) * size);
 
-  if (cr == NULL)
-    return (NULL);
+if (cr == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+cr[i] = c;
 
-  for (i = 0; i < size; i++)
-    cr[i] = c;
-
-  return (cr);
+return (cr);
 }

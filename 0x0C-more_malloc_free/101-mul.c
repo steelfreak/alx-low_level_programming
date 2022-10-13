98467d9e138f45073067db0add0889fea5ab8064
steelfreak <kinenkey@gmail.com>
2022-10-13 12:40:15 -0700

    Task 5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,32 +1,165 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
+
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
 
 /**
- * main - check the code for Holberton School students.
- * @argc: argument count.
- * @argv: argument vector.
+ * find_len - FInd the length of the string
+ * @str: The string to be measured.
  *
- * Return: Always 0.
+ *Return: The length of the string
  */
-
-int main(int argc, char *argv[])
+int find_len(char *str)
 {
-  unsigned long mul[];
-  int i, j;
-  if (argc != 3)
-    { printf("Error\n");
-      exit(98); }
-  for (i = 1; i < argc; i++)
-    {
-      for (j = 0; argv[i][j] != '\0'; j++)
-	{
-	  if (argv[i][j] > 57 || argv[i][j] < 48)
-	    {  printf("Error\n");
-	      exit(98); }
-	}
+int len = 0;
+  
+while(*str++)
+len++;
+  
+return (len);
+}
 
-    }
-  mul = atol(argv[1]) *  atol(argv[2]);
-  printf("%lu\n", mul);
-  return (0);
+/**
+ *create_xarray - creates an array of chars and initializes it with
+ *     the character 'x' -Adds a terminating null byte.
+ * @size: the size of the array to be initialized.
+ *
+ *Description: If there is insufficient space, the
+ *          function exits with a status of 98.
+ * Return:  A pointer to the Array.
+ */
+char *create_xarray(int size)
+{
+char *array;
+init index;
+
+array = malloc(sieof(char) *size);
+
+if (array == NULL)
+exit(98);
+
+for (index = 0; index < (size - 1); index++)
+array[index] = 'x';
+
+array[index] = '\0';
+
+return (array);
+}
+
+/**
+ * iterate_zeroes - Iterates through a string of numbers containing
+ *    leading zeroes until it hits a non zero number.
+ * @str: TheThe string of numbers to be iterated through.
+ *
+ * Return: A pointer to the next non-zero element.
+ */
+char *iterate_zeroes(char *str)
+{
+while (*str && *str == '0')
+str++;
+
+return (str);
+}
+
+/**
+ *get_digit - converts a digit character to a corresponding int.
+ *@c: The character to be converted.
+ *
+ * Description: If c is a non-digit, the function
+ *    exits with a status of 98.
+ * Return:  The converted int.
+ */
+int get_digit(char c)
+{
+int digit = c - '0';
+
+if (digit < 0 || digit > 9)
+{
+printf("Error\n");
+exit(98);
+}
+
+return (digit);
+}
+
+/**
+ * get_prod - Multiplies a string of numbers by a single digit.
+ * @prod: The buffer to store the results.
+ * @digit: The single digit.
+ * @mult: The single digit.
+ * zeroes: The necessary number of leading zeroes.
+ *
+ * Description: If mult contains a non-digit, the function
+ *        exits with a status of 98.
+ */
+void get_prod(char *prod, char *mult, int digit, int zeroes)
+{
+int mult_len, num, tens = 0;
+
+mult_len = find_len(mult) - 1;
+mult += mult_len;
+
+while (*prod)
+{
+*prod = 'x';
+prod++;
+}
+
+prod--;
+
+while (zeroes--)
+{
+*prod = '0';
+prod--;
+}
+
+for (; mult_len >= 0; muly_len--, mult--, prod--)
+{
+if (*mult < '0' || *mult > '9')
+{
+printf("Error\n");
+exit(98);
+}
+
+num = (*mult - '0') *digit;
+num += tens;
+*prod =(num % 10) + '0';
+tens = num / 10;
+}
+
+if (tens)
+*prod = (tens % 10) + '0';
+}
+
+/**
+ * add_nums - Adds the numbrs stored in two strings.
+ * @final_prod: The buffer storing the running final product.
+ * @next_prod: The next product to be added.
+ * @next_len:  The length of the next prod.
+ */
+void add_nums(char *final_prod, char *next_prod, int next_len)
+{
+int num, tens = 0;
+
+while (*(final_prod + 1))
+final_prod++;
+
+while (*(next_prod + 1))
+next_prod++;
+
+for (; *final_prod != 'x'; final_prod--)
+{
+num = (*final_prod - '0') + (*next_prod - '0');
+num += tens;
+*final_prod = (num % 10) + '0';
+tens = num / 10;
+
+next_prod--;
+next_len--;
+}
+
+for (;)
 }
